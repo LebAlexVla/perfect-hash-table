c99b8be387d560a6c598e168889c8c5739b58b40
Alexander Lebedev <lebalexvla@gmail.com>
2025-06-27 00:10:26 +0300

    Added Constructor and Find

--- lib/PerfectHashTable.hpp
+++ lib/PerfectHashTable.hpp
@@ -6,6 +6,7 @@
 #include <optional>
 #include <functional>
 #include <algorithm>
+#include <memory>
 
 #include "UniversalHash.hpp"
 
@@ -14,67 +15,92 @@ class PerfectHashTable {
 public:
     explicit PerfectHashTable(const std::vector<std::pair<Key, T>>& data)
     : size_(data.size()) {
-        std::vector<std::vector<size_t>> buckets_indices(size_);
-        BuildFirstLevel(buckets_indices);
-        BuildSecondLevel(data, buckets_indices);
+        std::vector<std::vector<size_t>> indices_in_bucket(size_);
+        BuildFirstLevel(data, indices_in_bucket);
+        BuildSecondLevel(data, indices_in_bucket);
     }
 
-    ~PerfectHashTable();
+    std::optional<std::reference_wrapper<const T>> Find(const Key& key) {
+        if (!size_)
+            return std::nullopt;
 
-    std::optional<std::reference_wrapper<const T>> Find(const Key& key);
-    std::optional<std::reference_wrapper<T>> At(const Key& key);
+        size_t first_index = first_hash_(key) % size_;
+        if (!buckets_[first_index])
+            return std::nullopt;
+
+        size_t second_index = buckets_[index]->second_hash(key) % buckets_[first_index]->data.size();
+        if (!buckets_[second_index])
+            return std::nullopt;
+
+        return std::cref(buckets_[first_index]->data[second_index].second);
+    }
+
+    std::optional<std::reference_wrapper<T>> At(const Key& key) {
+
+    }
+
     bool Erase(const Key& key);
-
-    PerfectHashTable(const PerfectHashTable&) = delete;
-    PerfectHashTable& operator=(const PerfectHashTable&) = delete;
 
 private:
     struct Bucket {
         UniversalHash<Key> second_hash;
-        std::vector<std::pair<Key, T>> data;
+        std::vector<std::optional<std::pair<const Key, T>>> data;
     };
 
-    void BuildFirstLevel(std::vector<std::vector<size_t>>& buckets_indices) {
+    void BuildFirstLevel(const std::vector<std::pair<Key, T>>& data, std::vector<std::vector<size_t>>& indices_in_bucket) {
         bool is_hashed = false;
         while (!is_hashed) {
             size_t i = 0;
             for (const auto& [key, value] : data) {
                 size_t index = first_hash_(key) % size_;
-                buckets_indices[index].push_back(i++);
+                indices_in_bucket[index].push_back(i++);
             }
 
             size_t sum_of_squares = 0;
-            for (const auto& bucket : buckets_indices)
+            for (const auto& bucket : indices_in_bucket)
                 sum_of_squares += bucket.size() * bucket.size();
             if (sum_of_squares <= 4 * size_) {
                 is_hashed = true;
             } else {
                 first_hash_.GenCoefs();
-                for (auto& bucket : buckets_indices)
+                for (auto& bucket : indices_in_bucket)
                     bucket.clear();
             }
         }
     }
 
-    void BuildSecondLevel(const std::vector<std::pair<Key, T>>& data, std::vector<size_t>& buckets_indices) {
-        for (size_t i = 0; i < buckets_indices.size(): ++i) {
-            buckets_[i].resize(buckets_indices[i].size() * buckets_indices[i].size());
+    void BuildSecondLevel(const std::vector<std::pair<Key, T>>& data, std::vector<std::vector<size_t>>& indices_in_bucket) {
+        buckets_.resize(size_);
+        for (size_t i = 0; i < indices_in_bucket.size(); ++i) {
+            if (indices_in_bucket[i].size() > 0) {
+                buckets_[i] = std::make_unique<Bucket>();
+                buckets_[i]->data.resize(indices_in_bucket[i].size() * indices_in_bucket[i].size());
+            }
         }
 
-        for (auto& bucket : buckets_indices) {
-            UniversalHash<Key> second_hash();
+        for (size_t i = 0; i < indices_in_bucket.size(); ++i) {
             bool is_hashed = false;
-            int i = 0;
             while (!is_hashed) {
-                auto& [key, value] = data[bucket[i]];
-                if ()
+                is_hashed = true;
+                for (size_t j = 0; j < indices_in_bucket[i].size(); ++j) {
+                    const auto& [key, value] = data[indices_in_bucket[i][j]];
+                    size_t index = buckets_[i]->second_hash(key) % buckets_[i]->data.size();
+                    if (!buckets_[i]->data[index]) {
+                        buckets_[i]->data[index] = {key, value};
+                    } else {
+                        is_hashed = false;
+                        buckets_[i]->second_hash.GenCoefs();
+                        buckets_[i]->data.clear();
+                        buckets_[i]->data.resize(indices_in_bucket[i].size() * indices_in_bucket[i].size());
+                        break;
+                    }
+                }
             }
-            std::vector<size_t>().swap(bucket);
+            std::vector<size_t>().swap(indices_in_bucket[i]);
         }
-        // TODO
     }
 
     UniversalHash<Key> first_hash_;
-    std::vector<Bucket> buckets_;
+    std::vector<std::unique_ptr<Bucket>> buckets_;
     size_t size_;
 };
